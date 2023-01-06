12018acb8e02cee1817782baf65fe34870b3d3e4
Azis Ramadhan <95456933+azeine@users.noreply.github.com>
2023-01-06 08:57:40 +0700

    Fix wrong image path causing build to fail

--- src/landing.vue
+++ src/landing.vue
@@ -9,8 +9,7 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
   <main>
     <picture>
       <source type="image/avif" srcset="./assets/images/me.avif"/>
-      <source type="image/png" srcset="./assets/images/me.png"/>
-      <img src="../assets/images/me.png" decoding="async" width="500" height="500" alt="My picture"/>
+      <img src="./assets/images/me.png" decoding="async" width="500" height="500" alt="My picture"/>
     </picture>
     <div class="intro">
       <h1 class="greet">Hi There. 👋</h1>
