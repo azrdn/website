2403f28009291929500f9f8e488406bc5df3394f
Azis Ramadhan <hi@azrd.dev>
2024-06-21 22:27:30 +0700

    its 2024

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { Features } from 'lightningcss'
+import minifyHTML from './plugin/minifyHtml'
+
+export default defineConfig({
+    appType: 'mpa',
+    plugins: [minifyHTML()],
+    css: {
+        transformer: 'lightningcss',
+        lightningcss: {
+            exclude: Features.Selectors
+        }
+    }
+})
