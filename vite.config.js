3345e813156e99e8936a87b8abb2846805c2d39e
Azis Ramadhan <hi@azrd.dev>
2024-07-07 14:35:02 +0700

    style, feat: add more tooltips, utilize margin collapse
    add editorconfig and removed a comment

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import { Features } from 'lightningcss'
 import minifyHTML from './plugin/minifyHtml'
 
 export default defineConfig({
-    appType: 'mpa',
-    plugins: [minifyHTML()],
-    css: {
-        transformer: 'lightningcss',
-        lightningcss: {
-            exclude: Features.Selectors | Features.VendorPrefixes
-        }
-    }
+	appType: 'mpa',
+	plugins: [minifyHTML()],
+	css: {
+		transformer: 'lightningcss',
+		lightningcss: {
+			exclude: Features.Selectors | Features.VendorPrefixes
+		}
+	}
 })
