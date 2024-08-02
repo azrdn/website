afdb62fb804964a0164a586ddb4f5a481c26c635
Azis Ramadhan <git@azrd.dev>
2024-08-02 20:11:17 +0700

    remove process.cwd()
    unnecesary because Bun.file() is already relative to cwd, also removed semicolons

--- plugin/devServer.ts
+++ plugin/devServer.ts
@@ -1,19 +1,17 @@
-import path from 'path';
-
-const initialDir = path.join(process.cwd(), process.argv[2] ?? '')
+import path from 'path'
 
 const server = Bun.serve({
     async fetch(req) {
-        let requestPath = new URL(req.url).pathname;
-        if (requestPath.endsWith('/')) requestPath += 'index.html';
-        const systemPath = path.join(initialDir, requestPath);
-        return new Response(Bun.file(systemPath));
+        let requestPath = new URL(req.url).pathname
+        if (requestPath.endsWith('/')) requestPath += 'index.html'
+        const combinedPath = path.join(process.argv[2] ?? '', requestPath)
+        return new Response(Bun.file(combinedPath))
     },
     error(request) {
-        console.error(request)
+        console.error(request.code, request.message)
         return new Response('Not found lol', { status: 404 })
     },
     port: 0,
-});
+})
 
-console.log(`Server started at ${server.url}`);
+console.log(`Server started at ${server.url}`)
