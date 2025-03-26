c38986c4e662e9a1550c895952b3d3025722b696
Azis Ramadhan <git@azrd.dev>
2025-03-26 17:50:11 +0700

    add blog

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	server: {
+		host: true,
+	},
+	markdown: {
+		shikiConfig: {
+			theme: 'github-dark-dimmed'
+		}
+	}
+});
---
+++ posts/long_sentence.md
@@ -0,0 +1,5 @@
+---
+title: a long sentence with some other unnecesary words that will test the layout sizing
+date: 1970-01-01
+---
+
