b43dd922bc480fecf8527257a1ed03958645e4b4
Azis Ramadhan <git@azrd.dev>
2025-06-02 19:00:32 +0700

    serve raw markdown
    - relaxed trailing slash config
    - add link to every post

---
+++ src/pages/blog/[...slug].md.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from 'astro';
+import { getCollection, getEntry } from "astro:content";
+
+export async function getStaticPaths() {
+	const posts = await getCollection("blog");
+	return posts.map((post) => ({
+		params: { slug: post.id }
+	}));
+}
+
+export const GET: APIRoute = async ({ params }) => {
+	const md = await getEntry("blog", params.slug as string);
+	if (!md) return new Response("Not Found");
+	return new Response(md.body);
+}
+
+//TODO: Favicon for non-HTML files
