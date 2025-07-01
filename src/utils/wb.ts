299621006ab2f0ce3d86f795721c4293a6fea6e7
Azis Ramadhan <git@azrd.dev>
2025-07-01 22:52:11 +0700

    programmatic? more like problematic
    his ass is not working!!!

--- src/utils/wb.ts
+++ src/utils/wb.ts
@@ -1,16 +1,17 @@
-import { type StdioOptions, spawn } from "node:child_process";
+import { exec, type StdioOptions, spawn } from "node:child_process";
 import { watch } from "node:fs";
-import { build } from "astro";
 
 const [command, ...args] =
-	process.argv[2] === "cf" ? ["wrangler", "dev"] : ["astro", "preview"];
+	process.argv[2] === "cf"
+		? ["wrangler", "dev", "--log-level=none"]
+		: ["astro", "preview"];
 const stdio: StdioOptions = ["ignore", "inherit", "inherit"];
-const child = spawn(command, args, { stdio });
 
+const server = spawn(command, args, { stdio });
 let build_ongoing = false;
 
 process.on("SIGINT", () => {
-	child.kill("SIGINT");
+	server.kill("SIGINT");
 	console.log("Exiting");
 	process.exit();
 });
@@ -19,6 +20,8 @@ watch(`${process.cwd()}/src`, { recursive: true }, async () => {
 	if (build_ongoing) return;
 
 	build_ongoing = true;
-	await build({ root: process.cwd() }, { teardownCompiler: false });
-	build_ongoing = false;
+	exec("astro build", { cwd: process.cwd() }, err => {
+		if (err) console.error("Build failed");
+		build_ongoing = false;
+	});
 });
