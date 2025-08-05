11df4f87b33b4cc9d4db40da46524412abe1c89b
Azis Ramadhan <git@azrd.dev>
2025-08-05 12:34:21 +0700

    add rss and move badges to about page

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,21 @@
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async (ctx) => {
+	const posts = await Promise.all([
+		getCollection("blog"),
+		getCollection("blog_mdx"),
+	]);
+
+	return rss({
+		title: "azrd",
+		description: "My personal site blog",
+		site: import.meta.env.SITE ?? `http://localhost:${ctx.url.port}`,
+		items: posts.flat().map(post => ({
+			title: post.data.title,
+			pubDate: post.data.createdAt,
+			link: `/blog/${post.id}`,
+		})),
+	});
+}
--- src/test-posts/latest.md
+++
@@ -1,6 +0,0 @@
----
-title: Latest post should be at the top
-createdAt: 2077-01-01
----
-
-This is the latest post. It should be at the top of the list because its sorted from the latest creation date.
