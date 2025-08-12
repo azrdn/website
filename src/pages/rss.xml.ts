1e6a8dbdc4ddc83146919db6d8b5975e570d5019
Azis Ramadhan <git@azrd.dev>
2025-08-12 16:30:36 +0700

    fix rss in dev

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,7 +2,7 @@ import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
 import type { APIRoute } from "astro";
 
-export const GET: APIRoute = async (ctx) => {
+export const GET: APIRoute = async ctx => {
 	const posts = await Promise.all([
 		getCollection("blog"),
 		getCollection("blog_mdx"),
@@ -11,11 +11,13 @@ export const GET: APIRoute = async (ctx) => {
 	return rss({
 		title: "azrd",
 		description: "My personal site blog",
-		site: import.meta.env.SITE ?? `http://localhost:${ctx.url.port}`,
+		site: import.meta.env.DEV
+			? `http://localhost:${ctx.url.port}`
+			: import.meta.env.SITE,
 		items: posts.flat().map(post => ({
 			title: post.data.title,
 			pubDate: post.data.createdAt,
 			link: `/blog/${post.id}`,
 		})),
 	});
-}
+};
