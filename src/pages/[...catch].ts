03090257fa228111e3bad9645383d9bc38a65e5c
Azis Ramadhan <git@azrd.dev>
2025-08-28 17:45:06 +0700

    bomb the vuln scanners

--- src/pages/[...catch].ts
+++ src/pages/[...catch].ts
@@ -1,12 +1,33 @@
 import type { APIRoute } from "astro";
 
-export const prerender = false
-export const GET: APIRoute = async (ctx) => {
-	const url = new URL(ctx.request.url)
+const BOMB_SIZE = import.meta.env.DEV ? "1K" : "10G";
+const is_malicious = (pathname: string): boolean => {
+	return (
+		pathname.startsWith("/wp-") ||
+		pathname.includes(".php") ||
+		pathname.includes(".env")
+	);
+};
 
-	const bomb = new Response("booom", { status: 418 })
-	if (url.pathname.startsWith("/wp")) return bomb
-	if (url.pathname.includes(".php")) return bomb
-	
-	return ctx.redirect("/404", 301)
+const check_encoding = (header: string | null) => {
+	if (header?.includes("br")) return "br"
+	if (header?.includes("gzip")) return "gzip"
+	return null
+}
+
+export const prerender = false;
+export const GET: APIRoute = async ({ request, locals, redirect }) => {
+	const url = new URL(request.url);
+
+	if (!is_malicious(url.pathname)) return redirect("/404", 301);
+	const encoding = check_encoding(request.headers.get("accept-encoding"))
+	if (!encoding) return redirect("https://hel1-speed.hetzner.com/10GB.bin", 301)
+
+	const zip_bomb = await locals.runtime.env.ASSETS.fetch(
+		`${url.origin}/zipbomb/${BOMB_SIZE}.${encoding}`,
+	);
+
+	return new Response(zip_bomb.body, {
+		headers: { "Content-Encoding": encoding },
+	});
 };
