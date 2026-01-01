7a50d52e867ab3e21ff2d72d0eacda858bafe3d3
Azis Ramadhan <git@azrd.dev>
2026-01-01 11:04:47 +0700

    refactor: simplify caching logic in cached fetch util

--- src/utils/cached_fetch.ts
+++ src/utils/cached_fetch.ts
@@ -4,32 +4,19 @@ import fs from "node:fs/promises"
 const cache_dir = ".cache"
 export default async (url: string, ttl_secs: number, init?: RequestInit) => {
 	const hash = createHash("md5").update(url).digest("hex")
-	await fs.mkdir(cache_dir, { recursive: true }).catch(() => {})
+	const file_path = `${cache_dir}/${hash}`
+	const stats = await fs.stat(file_path)
 
-	const files = await fs.readdir(cache_dir, { withFileTypes: true })
-	const now = Date.now()
-
-	for (const file of files) {
-		if (!file.isFile() || !file.name.startsWith(hash)) continue
-
-		const [, expiry_str = "0"] = file.name.split("-")
-		const expiry = Number.parseInt(expiry_str, 10)
-		const file_path = `${cache_dir}/${file.name}`
-
-		if (!Number.isFinite(expiry) || expiry <= now) {
-			await fs.rm(file_path).catch(() => {})
-			continue
-		}
-
+	if ((Date.now() - stats.mtimeMs) < (ttl_secs * 1000)) {
 		const cached = await fs.readFile(file_path)
 		return new Response(cached as BodyInit)
 	}
 
 	const res = await fetch(url, init)
 	const data = await res.arrayBuffer()
-	const expiry = Date.now() + ttl_secs * 1000
 
-	await fs.writeFile(`${cache_dir}/${hash}-${expiry}`, Buffer.from(data))
+	await fs.mkdir(cache_dir, { recursive: true })
+	await fs.writeFile(file_path, new Uint8Array(data))
 
 	return new Response(data, {
 		status: res.status,
