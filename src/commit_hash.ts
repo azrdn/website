4cb30c590ba492a298207884462fe348ae80266e
Azis Ramadhan <git@azrd.dev>
2026-02-19 23:52:54 +0700

    fix: do not hardcode output file for commit hash integration

--- src/commit_hash.ts
+++ src/commit_hash.ts
@@ -2,19 +2,19 @@ import type { AstroIntegration } from "astro"
 import { execSync } from "node:child_process"
 import { writeFileSync } from "node:fs"
 
-export default (): AstroIntegration => ({
+export default (outFile: string = "public/commit_hash.json"): AstroIntegration => ({
 	name: "commit-hash",
 	hooks: {
 		"astro:config:setup": () => {
-			const hash = execSync("git rev-parse --short HEAD",
+			const stdout = execSync("git rev-parse --short HEAD",
 				{
 					timeout: 2000,
 					encoding: "utf8",
 				}
-			).trim()
+			)
 			
-			writeFileSync("public/commit_hash.json", JSON.stringify({ hash }))
-			console.info("Written latest git commit hash to public directory.")
+			writeFileSync(outFile, JSON.stringify({ hash: stdout.trim() }))
+			console.info(`Written latest git commit hash to ${outFile}`)
 		}
 	}
 })
