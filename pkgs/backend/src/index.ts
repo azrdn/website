a7355887da424782d02c6f965b2dacc274ecd77c
Azis Ramadhan <git@azrd.dev>
2026-05-03 15:18:02 +0700

    feat: Add OpenAPI doc and rate limiting for backend

--- pkgs/backend/src/index.ts
+++ pkgs/backend/src/index.ts
@@ -1,11 +1,24 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysia/cors";
+import { openapi } from "@elysia/openapi";
 import { CloudflareAdapter } from "elysia/adapter/cloudflare-worker";
+import { createHash } from "node:crypto";
 import z from "zod";
 
 import { drizzle } from "drizzle-orm/postgres-js";
 import { desc } from "drizzle-orm";
 import { table } from "../db/schema";
+
+import { env } from "cloudflare:workers";
+
+const hasher = (str: string) => createHash("sha256").update(str).digest("hex");
+const ratelimiter = async (content: string, ip: string) => {
+	const [ipResult, contentResult] = await Promise.all([
+		env.IP.limit({ key: ip }),
+		env.CONTENT.limit({ key: hasher(content) }),
+	]);
+	return ipResult.success && contentResult.success;
+};
 
 const app = new Elysia({ adapter: CloudflareAdapter })
 	.use(
@@ -13,6 +26,7 @@ const app = new Elysia({ adapter: CloudflareAdapter })
 			origin: process.env.FRONTEND_URL,
 		}),
 	)
+	.use(openapi())
 	.get("/guestbook", async () => {
 		const db = drizzle(process.env.DATABASE_URL);
 		return await db.select().from(table).orderBy(desc(table.id));
@@ -20,6 +34,11 @@ const app = new Elysia({ adapter: CloudflareAdapter })
 	.post(
 		"/guestbook",
 		async ({ body, status, redirect, headers }) => {
+			const input = JSON.stringify(body);
+			const ip = headers["cf-connecting-ip"] ?? "";
+			const allowed = await ratelimiter(input, ip);
+			if (!allowed) return status(429);
+
 			const fetchMode = headers["sec-fetch-mode"];
 			const db = drizzle(process.env.DATABASE_URL);
 			await db.insert(table).values(body);
@@ -41,6 +60,7 @@ const app = new Elysia({ adapter: CloudflareAdapter })
 			}),
 			headers: z.object({
 				"sec-fetch-mode": z.string().nullish(),
+				"cf-connecting-ip": z.string().nullish(),
 			}),
 		},
 	)
