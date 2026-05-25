9de5104eb1e115d6557109896b61740a0fc09276
Azis Ramadhan <git@azrd.dev>
2026-05-25 16:27:44 +0700

    feat: include prod post in dev too

--- pkgs/frontend/src/content.config.ts
+++ pkgs/frontend/src/content.config.ts
@@ -1,20 +1,23 @@
 import { defineCollection } from "astro:content"
 import { glob } from "astro/loaders"
-import { cmdLoader } from "./cmd.loader"
+import { cmdLoader } from "./utils/cmd.loader"
 import { z } from "astro/zod"
 
-const posts_path = import.meta.env.PROD ? "../posts" : "./src/test/posts"
+const blog_schema = z.object({
+	title: z.string().min(1, "Title cannot be empty"),
+	createdAt: z.date(),
+	updatedAt: z.date().optional(),
+})
 
 export const collections = {
-	blog: defineCollection({
-		loader: glob({ pattern: "**/*.{md,mdx}", base: posts_path }),
-		schema: z.object({
-			title: z.string().min(1, "Title cannot be empty"),
-			createdAt: z.date(),
-			updatedAt: z.date().optional(),
-		})
+	test_blog: defineCollection({
+		loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/test/posts" }),
+		schema: blog_schema
 	}),
-
+	blog: defineCollection({
+		loader: glob({ pattern: "**/*.{md,mdx}", base: "../posts" }),
+		schema: blog_schema
+	}),
 	gitInfo: defineCollection({
 		loader: cmdLoader([
 			"git rev-parse --short HEAD",
