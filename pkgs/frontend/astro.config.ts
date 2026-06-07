64524f2fcc6496405081ba2ed5926153a7f0a49b
Azis Ramadhan <git@azrd.dev>
2026-06-07 13:50:04 +0700

    feat: changes
    - font and style changes
    - move icons to svg file
    - text size and line height change

--- pkgs/frontend/astro.config.ts
+++ pkgs/frontend/astro.config.ts
@@ -1,13 +1,13 @@
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import * as c from "astro/config"
-import rehypeExtLinks from "rehype-external-links"
-import minify from 'astro-minify-html-swc'
-import cloudflare from '@astrojs/cloudflare';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import * as c from "astro/config";
+import rehypeExtLinks from "rehype-external-links";
+import minify from "astro-minify-html-swc";
+import cloudflare from "@astrojs/cloudflare";
 
 export default c.defineConfig({
 	adapter: cloudflare({
-		imageService: "passthrough"
+		imageService: "passthrough",
 	}),
 	site: "https://azrd.dev",
 	integrations: [sitemap(), mdx(), minify()],
@@ -50,7 +50,7 @@ export default c.defineConfig({
 	fonts: [
 		{
 			provider: c.fontProviders.local(),
-			name: "myfont",
+			name: "Ioskeley Mono",
 			fallbacks: ["monospace"],
 			cssVariable: "--font-subset",
 			options: {
@@ -58,16 +58,18 @@ export default c.defineConfig({
 					{
 						weight: "400 700",
 						style: "normal",
-						src: ["./src/styles/font.woff2"],
+						featureSettings: `"ss01"`,
+						src: ["./src/styles/Ioskeley-90.woff2"],
 					},
 					{
 						weight: "400 700",
 						style: "oblique",
-						variationSettings: "'slnt' -12",
-						src: ["./src/styles/font.woff2"],
+						featureSettings: `"ss01"`,
+						variationSettings: `"slnt" -12`,
+						src: ["./src/styles/Ioskeley-90.woff2"],
 					},
-				]
-			}
+				],
+			},
 		},
 	],
-})
+});
